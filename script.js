e8905487031e8a561627e3688fff8eb25ed84a34
llenn0210 <egieboybanac10@gmail.com>
2025-08-21 23:34:37 +0800

    new changes

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
+function hideAllSections() {
+    document.getElementById('about-section').style.display = 'none';
+    document.getElementById('name-section').style.display = 'none';
+    document.getElementById('contact-section').style.display = 'none';
+    
+    document.getElementById('skills-section').style.display = 'none';
+    
+}
+hideAllSections();
+document.getElementById('name-section').style.display = 'block';
+
 document.querySelector('.education-link').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('about-section').style.display = 'none';
-     document.getElementById('name-section').style.display = 'none';
+    hideAllSections();
+    document.getElementById('education-section').style.display = 'block';
 });
 
 document.querySelector('.skills-link').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('about-section').style.display = 'none';
-    document.getElementById('name-section').style.display = 'none';
+    hideAllSections();
+    document.getElementById('skills-section').style.display = 'block';
+
 });
 
 document.querySelector('.about-link').addEventListener('click', function(event) {
     event.preventDefault();
+    hideAllSections();
     document.getElementById('about-section').style.display = 'block';
-     document.getElementById('name-section').style.display = 'none';
 });
 
 document.querySelector('.contact-link').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('about-section').style.display = 'none';
-    document.getElementById('name-section').style.display = 'none';
+    hideAllSections();
+    document.getElementById('contact-section').style.display = 'block';
 });
 
 document.querySelector('.name-link').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('about-section').style.display = 'none';
+    hideAllSections();
     document.getElementById('name-section').style.display = 'block';
     
 });
